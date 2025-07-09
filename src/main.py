30950b56c01ea4165ea559a7856d77beb0d0942d
maurilima93 <mauri.lima93@gmail.com>
2025-07-08 23:48:25 -0300

    alter password aws rds2

--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ def create_app():
         db_port = os.getenv('DB_PORT', '5432')
         db_name = os.getenv('DB_NAME', 'personal-feed')
         db_user = os.getenv('DB_USER', 'admin1')
-        db_password = os.getenv('DB_PASSWORD', 'Ruffus11!')
+        db_password = os.getenv('DB_PASSWORD', 'EsNvDJNCydIdbKXcAZy5')
         
         app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}?sslmode=require'
     
