ad10af04cb9c11abf79130116f0aa40e86aa2ce7
maurilima93 <mauri.lima93@gmail.com>
2025-08-04 02:24:34 -0300

    fix 21

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ def create_app():
     # Configuração do banco de dados AWS RDS
     db_host = os.environ.get('DB_HOST', 'personal-feed.c3yc0my6ywu9.sa-east-1.rds.amazonaws.com')
     db_port = os.environ.get('DB_PORT', '5432')
-    db_name = os.environ.get('DB_NAME', 'postgres')
+    db_name = os.environ.get('DB_NAME', 'personal-feed')
     db_user = os.environ.get('DB_USER', 'admin1')
     db_password = os.environ.get('DB_PASSWORD', 'Ruffus11!')
     
