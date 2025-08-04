4e23cb4ca582e2fc52fe48f97cb1ef69e0c5a197
maurilima93 <mauri.lima93@gmail.com>
2025-08-04 02:27:24 -0300

    fix 22

--- src/main.py
+++ src/main.py
@@ -21,9 +21,9 @@ def create_app():
     # Configuração do banco de dados AWS RDS
     db_host = os.environ.get('DB_HOST', 'personal-feed.c3yc0my6ywu9.sa-east-1.rds.amazonaws.com')
     db_port = os.environ.get('DB_PORT', '5432')
-    db_name = os.environ.get('DB_NAME', 'personal-feed')
+    db_name = os.environ.get('DB_NAME', 'postgres')
     db_user = os.environ.get('DB_USER', 'admin1')
-    db_password = os.environ.get('DB_PASSWORD', 'Ruffus11!')
+    db_password = os.environ.get('DB_PASSWORD', 'EsNvDJNCydIdbKXcAZy5')
     
     app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}?sslmode=require'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
